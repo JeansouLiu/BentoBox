ecf4daecb82013fb49b48db69b84ca6256988c4c
Jeansou <bettertreebot@gmail.com>
2020-07-02 01:15:41 +0800

    Add files via upload

--- src/main/java/world/bentobox/bentobox/blueprints/BlueprintPaster.java
+++ src/main/java/world/bentobox/bentobox/blueprints/BlueprintPaster.java
@@ -274,9 +274,9 @@ public class BlueprintPaster {
             }
         } catch (IllegalArgumentException e) {
             // This may happen if the block type is no longer supported by the server
-            plugin.logWarning("Blueprint references materials not supported on this server version.");
-            plugin.logWarning("Load blueprint manually, check and save to fix for this server version.");
-            plugin.logWarning("World: " + world.getName() + "; Failed block data: " + block.getBlockData());
+            plugin.logWarning("蓝图文件中含有更新版本 Minecraft 的物品.");
+            plugin.logWarning("请手动加载蓝图文件, 移除不支持的物品.");
+            plugin.logWarning("世界: " + world.getName() + "; 无效方块数据: " + block.getBlockData());
         }
         return blockData;
     }
